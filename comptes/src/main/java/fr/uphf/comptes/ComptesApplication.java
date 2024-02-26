95040d1e6b349b020bc79253fe90b2ff3850e74a
MrADOY <pietrzak.aurelien@gmail.com>
2024-02-26 18:55:28 +0100

    ajout du client comptes et cartes

---
+++ comptes/src/main/java/fr/uphf/comptes/ComptesApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.comptes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComptesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComptesApplication.class, args);
+	}
+
+}
